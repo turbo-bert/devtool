d874280973ce457acb457b11bbf40d090ede1ef1
Robert Degen <robert.degen@mailbox.org>
2023-10-13 13:30:12 +0200

    ...

--- src/macos-sonoma.sh
+++ src/macos-sonoma.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 
+
+github_sh_to_bin() {
+    curl -L -o ~/bin/$1 "$2"
+    chmod a+x ~/bin/$1
+}
+
+
 if [[ ! -d ~/bin ]]; then
     mkdir -p ~/bin
+fi
+
+if [[ $1 = u ]]; then
+    github_sh_to_bin osd 'https://raw.githubusercontent.com/turbo-bert/osd/main/osd.sh'
+    github_sh_to_bin ds 'https://raw.githubusercontent.com/turbo-bert/osd/main/ds.sh'
 fi
